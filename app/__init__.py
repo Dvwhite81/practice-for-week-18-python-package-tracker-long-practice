0022d992cfd3731f9918c775984d50161e014c8d
Dvwhite81 <Dvwhite1981@gmail.com>
2023-09-10 15:09:01 -0400

    Complete - No bonus for now

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_migrate import Migrate
 from .config import Config
 from .routes import packages
+from app.models import db, Package
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.register_blueprint(packages.bp)
+db.init_app(app)
+migrate = Migrate(app, db)
--- app/routes/packages.py
+++ app/routes/packages.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, redirect, render_template, url_for
 from app.forms.shipping_form import ShippingForm
+from app.models import db, Package
 
 bp = Blueprint('packages', __name__, url_prefix='')
 
 
 @bp.route('/')
 def index():
-    return 'Package Tracker'
+    packages = Package.query.all()
+    return render_template('package_status.html', packages=packages)
 
 
 
@@ -14,6 +16,17 @@ def index():
 def new_package():
     form = ShippingForm()
     if form.validate_on_submit():
+        data = form.data
+        new_package = Package(
+            sender=data["sender"],
+            recipient=data["recipient"],
+            origin=data["origin"],
+            destination=data["destination"],
+            location=data["origin"]
+        )
+        db.session.add(new_package)
+        db.session.commit()
+        Package.advance_all_locations()
         return redirect(url_for('.index'))
 
     return render_template('shipping_request.html', form=form)
